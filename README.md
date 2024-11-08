8e9563a721a2bb6f78b2c8e8f96ec4e7d1bec3f2
Michael <m.nwuju@alustudent.com>
2024-11-08 14:38:09 +0200

    Set up Amakuru search flow

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# amakuru
-Our Amakuru project designed to provide career roadmaps to users based on their desired career of choice
+# Amakuru - Empowering Women in Tech
+
+Amakuru is a simple Python-based menu-driven application that empowers women in tech by providing detailed career roadmaps for various tech paths. By leveraging data from roadmap.sh and Guides with Mosh Hamedani, Amakuru helps users discover the knowledge and career paths they need to succeed in the tech industry.
+
+## Background
+
+The name "Amakuru" means "Knowledge" in Kinyarwanda, reflecting the project's mission to remain authentic and resonate with African roots. This tool serves as an empowering resource for women in tech, facilitating job creation and career growth.
+
+## Features
+
+- Searches for career paths using regex-based matching.
+- Display relevant career roadmaps and provide links for further detailed guides.
+- Integration with roadmap.sh and Mosh Hamedani's resources.
+- Easy-to-use command: `amakuru start`.
+
+## Prerequisites
+
+- Python 3.x installed on your system.
+- Pip installed on your system
+
+## Installation
+
+```bash
+pip install amakuru
+```
+
+## How to Run Amakuru
+
+```bash
+   amakuru start
+```
---
+++ amakuru/database.py
@@ -0,0 +1,84 @@
+career_paths = [
+    "Frontend Engineer",
+    "Backend Engineer",
+    "Fullstack Developer",
+    "Data Scientist",
+    "DevOps Engineer",
+    "UX Designer",
+    "AI Engineer",
+    "Technical Writer",
+    "Product Manager",
+    "Cyber Security",
+    "Blockchain Developer",
+    "Data Analyst",
+    "Android Developer",
+    "iOS Developer",
+]
+
+roadmaps = {
+    career_paths[0]: {
+        "link": "https://roadmap.sh/frontend",
+        "video": "https://youtu.be/Tef1e9FiSR0?si=T5OI1xEdP0O0I-ZH",
+        "guide": "https://guides.codewithmosh.com/frontend-developer-roadmap",
+    },
+    career_paths[1]: {
+        "link": "https://roadmap.sh/backend",
+        "video": "https://youtu.be/OeEHJgzqS1k?si=HBeuUsHnqwvwQza1",
+        "guide": "https://guides.codewithmosh.com/backend-developer-roadmap",
+    },
+    career_paths[2]: {
+        "link": "https://roadmap.sh/full-stack",
+        "video": "https://youtu.be/GxmfcnU3feo?si=qwHZk1vN2RMicG62",
+        "guide": "https://guides.codewithmosh.com/web-developer-roadmap",
+    },
+    career_paths[3]: {
+        "link": "https://roadmap.sh/ai-data-scientist",
+        "video": "https://youtu.be/9R3X0JoCLyU?si=jWFROS0wn04XuUMi",
+        "guide": "https://guides.codewithmosh.com/data-science-roadmap",
+    },
+    career_paths[4]: {
+        "link": "https://roadmap.sh/devops",
+        "video": "https://youtu.be/6GQRb4fGvtk?si=RwDYgoCFpZzyw8CJ",
+        "guide": "https://guides.codewithmosh.com/devops-roadmap",
+    },
+    career_paths[5]: {
+        "link": "https://roadmap.sh/ux-design",
+        "video": "https://youtu.be/HmKwiEmJIdM?si=ALh-JlXxf_sIkeSC",
+    },
+    career_paths[6]: {
+        "link": "https://roadmap.sh/ai-engineer",
+        "video": "https://youtu.be/7IgVGSaQPaw?si=CFb9kPNvmXu9VVdx",
+        "guide": "https://guides.codewithmosh.com/machine-learning-engineer-roadmap",
+    },
+    career_paths[7]: {
+        "link": "https://roadmap.sh/technical-writer",
+        "video": "https://youtu.be/1w1QYR-n1js?si=q--a2oixTNsBXxmS",
+    },
+    career_paths[8]: {
+        "link": "https://roadmap.sh/product-manager",
+        "video": "https://youtu.be/kD2vTLNHkhk?si=0tGv5l3Ps8IrIQAe",
+    },
+    career_paths[9]: {
+        "link": "https://roadmap.sh/cyber-security",
+        "video": "https://youtu.be/D4fYyu305jg?si=1m_sFRYXrjF7gKYv",
+    },
+    career_paths[10]: {
+        "link": "https://roadmap.sh/blockchain",
+        "video": "https://youtu.be/zglv3lCchSo?si=Ecrya2N2boweEJjk",
+    },
+    career_paths[11]: {
+        "link": "https://roadmap.sh/data-analyst",
+        "video": "https://youtu.be/YRJbhFLLPyE?si=E_gA8-iHSDrXNQj_",
+        "guide": "https://guides.codewithmosh.com/data-analyst-roadmap",
+    },
+    career_paths[12]: {
+        "link": "https://roadmap.sh/android",
+        "video": "https://youtu.be/yye7rSsiV6k?si=iT1sJ1ZTSqrE_3ej",
+        "guide": "https://guides.codewithmosh.com/mobile-app-developer-roadmap",
+    },
+    career_paths[13]: {
+        "link": "https://roadmap.sh/ios",
+        "video": "https://youtu.be/yye7rSsiV6k?si=iT1sJ1ZTSqrE_3ej",
+        "guide": "https://guides.codewithmosh.com/mobile-app-developer-roadmap",
+    },
+}
